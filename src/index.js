90f1104c4bb131cfcd329488fe10466520289d1c
MWDelaney <michael.delaney@gmail.com>
2022-05-02 08:50:22 -0400

    Fix formatting

--- src/index.js
+++ src/index.js
@@ -35,112 +35,112 @@ registerBlockStyle('rebel/aligned-column', {
  *
  * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
  */
-registerBlockType( 'rebel/aligned-column', {
-	/**
-	 * This is the display title for your block, which can be translated with `i18n` functions.
-	 * The block inserter will show this name.
-	 */
-	title: __( 'Aligned Column', 'create-block' ),
+registerBlockType('rebel/aligned-column', {
+  /**
+   * This is the display title for your block, which can be translated with `i18n` functions.
+   * The block inserter will show this name.
+   */
+  title: __('Aligned Column', 'create-block'),
 
-	/**
-	 * This is a short description for your block, can be translated with `i18n` functions.
-	 * It will be shown in the Block Tab in the Settings Sidebar.
-	 */
-	description: __(
-		'',
-		'create-block'
-	),
+  /**
+   * This is a short description for your block, can be translated with `i18n` functions.
+   * It will be shown in the Block Tab in the Settings Sidebar.
+   */
+  description: __(
+    '',
+    'create-block'
+  ),
 
-	/**
-	 * Blocks are grouped into categories to help users browse and discover them.
-	 * The categories provided by core are `common`, `embed`, `formatting`, `layout` and `widgets`.
-	 */
-	category: 'layout',
+  /**
+   * Blocks are grouped into categories to help users browse and discover them.
+   * The categories provided by core are `common`, `embed`, `formatting`, `layout` and `widgets`.
+   */
+  category: 'layout',
 
-	/**
-	 * An icon property should be specified to make it easier to identify a block.
-	 * These can be any of WordPress’ Dashicons, or a custom svg element.
-	 */
-	icon: 'align-none',
+  /**
+   * An icon property should be specified to make it easier to identify a block.
+   * These can be any of WordPress’ Dashicons, or a custom svg element.
+   */
+  icon: 'align-none',
 
-	/**
-	 * Optional block extended support features.
-	 */
-	supports: {
-		// Removes support for an HTML mode.
+  /**
+   * Optional block extended support features.
+   */
+  supports: {
+    // Removes support for an HTML mode.
     html: false,
   },
   attributes: {
-		columnWidth: {
-			type: 'integer',
-			default: 50
+    columnWidth: {
+      type: 'integer',
+      default: 50
     },
-		columnWidthEdit: {
-			type: 'string',
-			default: '50%'
+    columnWidthEdit: {
+      type: 'string',
+      default: '50%'
     },
   },
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
-	 *
-	 * @param {Object} [props] Properties passed from the editor.
-	 *
-	 * @return {WPElement} Element to render.
-	 */
+  /**
+   * The edit function describes the structure of your block in the context of the editor.
+   * This represents what the editor will render when the block is used.
+   *
+   * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+   *
+   * @param {Object} [props] Properties passed from the editor.
+   *
+   * @return {WPElement} Element to render.
+   */
 
-	edit: ( props ) => {
+  edit: (props) => {
 
     const { attributes, setAttributes } = props;
     const columnWidthEdit = attributes.columnWidth + "%";
 
-		return (
-			<div className={ props.className }>
-       <InspectorControls>
-        <PanelBody
-              title="Column Width"
-              icon=""
-              initialOpen={ true }
+    return (
+      <div className={props.className}>
+        <InspectorControls>
+          <PanelBody
+            title="Column Width"
+            icon=""
+            initialOpen={true}
           >
-          <RangeControl
-            label="Column Width"
-            value={attributes.columnWidth}
-            min={1}
-            max={100}
-            onChange={value => {
-              setAttributes({ columnWidth: value })
-              setAttributes({ columnWidthEdit: value + "%"});
-            }}
-          />
+            <RangeControl
+              label="Column Width"
+              value={attributes.columnWidth}
+              min={1}
+              max={100}
+              onChange={value => {
+                setAttributes({ columnWidth: value })
+                setAttributes({ columnWidthEdit: value + "%" });
+              }}
+            />
           </PanelBody>
         </InspectorControls>
         <div className="aligned-column-inner" style={{ flexBasis: columnWidthEdit }}>
           <InnerBlocks />
         </div>
-			</div>
-		);
-	},
+      </div>
+    );
+  },
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by the block editor into `post_content`.
-	 *
-	 * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
-	 *
-	 * @return {WPElement} Element to render.
-	 */
-	save: ( props ) => {
+  /**
+   * The save function defines the way in which the different attributes should be combined
+   * into the final markup, which is then serialized by the block editor into `post_content`.
+   *
+   * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
+   *
+   * @return {WPElement} Element to render.
+   */
+  save: (props) => {
     const columnWidth = props.attributes.columnWidthEdit;
 
-		return (
-			<div className={ props.className }>
+    return (
+      <div className={props.className}>
         <div className="aligned-column-inner" style={{ flexBasis: columnWidth }}>
           <InnerBlocks.Content />
         </div>
-			</div>
-		);
-	},
+      </div>
+    );
+  },
 });
